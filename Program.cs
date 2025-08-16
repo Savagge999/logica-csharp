dafbac498247954fe92037fe736842abad06a02e
Anderson Sergio Dev <Andersonsergio501@hotmail.com>
2025-08-16 15:44:40 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Olá_Mundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, Mundo!");
+            Console.WriteLine("Me livrei da maldição!");
+        }
+    }
+}
