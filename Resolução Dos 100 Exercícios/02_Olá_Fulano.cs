28ecb4520a577328a9c1ae52e294595f22e9d611
Anderson Sergio Dev <Andersonsergio501@hotmail.com>
2025-08-17 14:55:28 -0300

    Add files via upload

---
+++ Resolução Dos 100 Exercícios/02_Olá_Fulano.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Olá_Fulano
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é o seu nome? ");
+            string? nome = Console.ReadLine();
+            Console.WriteLine($"Olá {nome}, é um prazer te conhecer!");
+        }
+    }
+}
