388c5b9529ea592a17b7e90c12ba912a78b28c18
Anderson Sergio Dev <Andersonsergio501@hotmail.com>
2025-08-17 15:02:00 -0300

    add comments to code

--- Resolução Dos 100 Exercícios/02_Olá_Fulano.cs
+++ Resolução Dos 100 Exercícios/02_Olá_Fulano.cs
@@ -6,9 +6,11 @@ namespace Olá_Fulano
     {
         static void Main(string[] args)
         {
+            // Solicita o nome do usuário
             Console.Write("Qual é o seu nome? ");
             string? nome = Console.ReadLine();
             Console.WriteLine($"Olá {nome}, é um prazer te conhecer!");
         }
     }
+
 }
