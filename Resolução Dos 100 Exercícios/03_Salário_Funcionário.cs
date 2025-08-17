7cabdb230fe80d48fe0312d064599c708f311eac
Anderson Sergio Dev <Andersonsergio501@hotmail.com>
2025-08-17 14:59:34 -0300

    Add files via upload

---
+++ Resolução Dos 100 Exercícios/03_Salário_Funcionário.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Salário_Funcionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Chama o método para solicitar os dados do funcionário
+            SolicitarDadosFuncionario();
+        }
+
+        static void SolicitarDadosFuncionario()
+        {
+            // Solicita o nome e o salário do funcionário
+            Console.Write("Digite seu nome: ");
+            string? nome = Console.ReadLine();
+            Console.Write("Digite o seu Salário: ");
+            string? salario = Console.ReadLine();
+            
+            // Exibe os dados informados
+            Console.WriteLine($"O funcionário {nome}, tem um salário de R${salario} em Junho.");
+        }
+    }
+}
